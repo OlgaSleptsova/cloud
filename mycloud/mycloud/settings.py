1e227a786d935363a11e3f32e2335e3abc468003
Olga <osleptsova@gmail.com>
2026-08-05 23:17:51 +0300

    two commit

--- mycloud/mycloud/settings.py
+++ mycloud/mycloud/settings.py
@@ -104,9 +104,14 @@ WSGI_APPLICATION = 'mycloud.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mycloud',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
         'USER': 'postgres',
-        'PASSWORD':'123Qweasd'
+        'PASSWORD': '123Qweasd',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    
+       
     }
 }
 
@@ -145,7 +150,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
